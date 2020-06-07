fb9fa5c7f4cae4c8f53c97088fdbac550e8e354d
didierdelhaisse <did35@yahoo.com>
2020-06-07 09:11:44 +0200

    Code refactored

--- NSLayout/Controller/ViewController.swift
+++ NSLayout/Controller/ViewController.swift
@@ -11,9 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     weak var testView: UIView!
-    weak var button1: UIButton!
-    weak var button2: UIButton!
-    weak var button3: UIButton!
+    
+    let customButton = CustomButton()
     
     // MARK: - LoadView
     override func loadView() {
@@ -30,45 +29,10 @@ class ViewController: UIViewController {
             testView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
         ])
         self.testView = testView
-        
-        let button1 = UIButton(frame: .zero)
-        button1.setBackgroundImage(UIImage(named: "acButton"), for: .normal)
-        button1.translatesAutoresizingMaskIntoConstraints = false
-        self.testView.addSubview(button1)
-        self.testView.bringSubviewToFront(button1)
-        NSLayoutConstraint.activate([
-            button1.widthAnchor.constraint(equalToConstant: 60),
-            button1.heightAnchor.constraint(equalToConstant: 30),
-            button1.centerXAnchor.constraint(equalTo: self.testView.centerXAnchor),
-            button1.centerYAnchor.constraint(equalTo: self.testView.centerYAnchor, constant: -40)
-        ])
-        self.button1 = button1
-        
-        let button2 = UIButton(frame: .zero)
-        button2.setBackgroundImage(UIImage(named: "dcButton"), for: .normal)
-        button2.translatesAutoresizingMaskIntoConstraints = false
-        self.testView.addSubview(button2)
-        self.testView.bringSubviewToFront(button2)
-        NSLayoutConstraint.activate([
-            button2.widthAnchor.constraint(equalToConstant: 60),
-            button2.heightAnchor.constraint(equalToConstant: 30),
-            button2.centerXAnchor.constraint(equalTo: self.testView.centerXAnchor),
-            button2.centerYAnchor.constraint(equalTo: self.testView.centerYAnchor)
-        ])
-        self.button2 = button2
-        
-        let button3 = UIButton(frame: .zero)
-        button3.setBackgroundImage(UIImage(named: "stbyButton"), for: .normal)
-        button3.translatesAutoresizingMaskIntoConstraints = false
-        self.testView.addSubview(button3)
-        self.testView.bringSubviewToFront(button3)
-        NSLayoutConstraint.activate([
-            button3.widthAnchor.constraint(equalToConstant: 60),
-            button3.heightAnchor.constraint(equalToConstant: 30),
-            button3.centerXAnchor.constraint(equalTo: self.testView.centerXAnchor),
-            button3.centerYAnchor.constraint(equalTo: self.testView.centerYAnchor, constant: 40)
-        ])
-        self.button3 = button3
+        customButton.setButton1(title: "acButton", view: testView)
+        customButton.setButton2(title: "dcButton", view: testView)
+        customButton.setButton3(title: "stbyButton", view: testView)
+    
     }
     
     // MARK: - ViewDidLoad
@@ -76,25 +40,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         testView.backgroundColor = .red
-        addActionButton()
-    }
-    
-    func addActionButton() {
-        button1.addTarget(self, action: #selector(button1Tapped), for: .touchUpInside)
-        button2.addTarget(self, action: #selector(button2Tapped), for: .touchUpInside)
-        button3.addTarget(self, action: #selector(button3Tapped), for: .touchUpInside)
-    }
-    
-    @objc func button1Tapped() {
-        print("AC button was tapped")
-    }
-    
-    @objc func button2Tapped() {
-        print("DC button was tapped")
-    }
-    
-    @objc func button3Tapped() {
-        print("STBY button was tapped")
+        customButton.addActionButton()
     }
 }
 
--- NSLayout/Model/CustomButton.swift
+++ NSLayout/Model/CustomButton.swift
@@ -6,4 +6,82 @@
 //  Copyright © 2020 Didier Delhaisse. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CustomButton: UIButton {
+    
+    weak var button1: UIButton!
+    weak var button2: UIButton!
+    weak var button3: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+    }
+    
+    func setButton1(title: String, view: UIView) {
+        let button1 = UIButton(frame: .zero)
+        button1.setBackgroundImage(UIImage(named: title), for: .normal)
+        button1.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button1)
+        view.bringSubviewToFront(button1)
+        NSLayoutConstraint.activate([
+            button1.widthAnchor.constraint(equalToConstant: 60),
+            button1.heightAnchor.constraint(equalToConstant: 30),
+            button1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button1.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -40)
+        ])
+        self.button1 = button1
+    }
+    
+    func setButton2(title: String, view: UIView) {
+        let button2 = UIButton(frame: .zero)
+        button2.setBackgroundImage(UIImage(named: title), for: .normal)
+        button2.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button2)
+        view.bringSubviewToFront(button2)
+        NSLayoutConstraint.activate([
+            button2.widthAnchor.constraint(equalToConstant: 60),
+            button2.heightAnchor.constraint(equalToConstant: 30),
+            button2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button2.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        self.button2 = button2
+    }
+    
+    func setButton3(title: String, view: UIView) {
+        let button3 = UIButton(frame: .zero)
+        button3.setBackgroundImage(UIImage(named: title), for: .normal)
+        button3.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button3)
+        view.bringSubviewToFront(button3)
+        NSLayoutConstraint.activate([
+            button3.widthAnchor.constraint(equalToConstant: 60),
+            button3.heightAnchor.constraint(equalToConstant: 30),
+            button3.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button3.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 40)
+        ])
+        self.button3 = button3
+    }
+    
+    func addActionButton() {
+        button1.addTarget(self, action: #selector(button1Tapped), for: .touchUpInside)
+        button2.addTarget(self, action: #selector(button2Tapped), for: .touchUpInside)
+        button3.addTarget(self, action: #selector(button3Tapped), for: .touchUpInside)
+    }
+    
+    @objc func button1Tapped() {
+        print("AC button was tapped")
+    }
+    
+    @objc func button2Tapped() {
+        print("DC button was tapped")
+    }
+    
+    @objc func button3Tapped() {
+        print("STBY button was tapped")
+    }
+}
